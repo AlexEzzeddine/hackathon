97864b5467f504cab981f045e664d1c845fde567
alex <alex.izzedin@gmail.com>
2017-07-28 19:43:28 -0700

    disabled controls responsive

--- map.js
+++ map.js
@@ -1,27 +1,33 @@
-function calculateCenter()
+var map;
+function moveToCurLoc()
 {
-	
+	var pos;
+	var im = 'circle.png';
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			}
+			map.setCenter(pos);
+			var userMarker = new google.maps.Marker({
+				position: pos,
+				map: map,
+				icon: im
+			});
+		})
+	}
 }
 
 function initMap() {
-				var pos;
-				var im = 'circle.png';
-				var map = new google.maps.Map(document.getElementById('map'), {
-					zoom: 15,
-					center: {lat: -34.397, lng: 150.644}
-				});
-				if (navigator.geolocation) {
-					navigator.geolocation.getCurrentPosition(function(position) {
-						pos = {
-							lat: position.coords.latitude,
-							lng: position.coords.longitude
-						}
-						map.setCenter(pos);
-						var userMarker = new google.maps.Marker({
-								position: pos,
-								map: map,
-								icon: im
-						});
-					})
-				}
-			}
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: {lat: -34.397, lng: 150.644},
+		disableDefaultUI: true
+	});
+	moveToCurLoc();
+}
+
+window.onresize = function() {
+	moveToCurLoc();
+}
