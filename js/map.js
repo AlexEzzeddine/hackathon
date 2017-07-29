eaa5ed4a34bb3c4e6d755037dcf59e87ff8ad767
Anurag Mittal <anurag.213@gmail.com>
2017-07-29 02:19:19 -0700

    added layers, need to edit and make the beautiful

--- js/map.js
+++ js/map.js
@@ -31,4 +31,12 @@ function initMap() {
 window.onresize = function() {
 	moveToCurLoc();
 }
+
 $('.collapse').collapse()
+    $(".panel-collapse").on("hide.bs.collapse", function () {
+        $(".panel-collapse-clickable").find('i').removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+    });
+
+    $(".panel-collapse").on("show.bs.collapse", function () {
+        $(".panel-collapse-clickable").find('i').removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+    });
