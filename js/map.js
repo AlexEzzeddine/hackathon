f4f2fa0968bbc66661c4622c3ed629895404778d
Alex <alex.izzedin@gmail.com>
2017-07-30 04:56:40 -0700

    style fixes

--- js/map.js
+++ js/map.js
@@ -3,43 +3,35 @@ var request_markers = [];
 
 client = Rapid.createClient("NDA1OWE0MWo1b3AzYzA3LnJhcGlkLmlv")
 
- function CustomMarker(pos,  map, image) {
+ function CustomMarker(pos,  map, el) {
     this.latlng_ = new google.maps.LatLng(pos.lat, pos.lng);
-    this.image = image
+    this.el = el
     this.setMap(map);
   }
 
   CustomMarker.prototype = new google.maps.OverlayView();
   CustomMarker.prototype.draw = function() {
     var me = this;
-    var div = this.div_;
-    if (!div) {
-      div = this.div_ = document.createElement('DIV');
-      div.style.border = "none";
-      div.style.position = "absolute";
-      div.style.paddingLeft = "0px";
-      div.style.cursor = 'pointer';
-      //you could/should do most of the above via styling the class added below
-		$(div).append(this.image);
-      google.maps.event.addDomListener(div, "click", function(event) {
+    var el = this.el;
+
+    google.maps.event.addDomListener(el, "click", function(event) {
         google.maps.event.trigger(me, "click");
       });
 
       var panes = this.getPanes();
-      panes.overlayImage.appendChild(div);
-    }
+      panes.overlayImage.appendChild(el);
 
     var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
     if (point) {
-      div.style.left = point.x + 'px';
-      div.style.top = point.y + 'px';
+      el.style.left = point.x + 'px';
+      el.style.top = point.y + 'px';
     }
   };
 
   CustomMarker.prototype.remove = function() {
-    if (this.div_) {
-      this.div_.parentNode.removeChild(this.div_);
-      this.div_ = null;
+    if (this.el) {
+      this.el.parentNode.removeChild(this.el);
+      this.el = null;
     }
   };
 
@@ -59,7 +51,7 @@ function placeFoodRequest() {
 			.collection('food_requests')
 			.newDocument() // generate the id of a new document automatically
 			.mutate({
-				name: $("#request-name").val(),
+				name: user.body.name,
 				time: $("#request-time").val(),
 				food: $("#request-food").val(),
 				image: user.body.image,
@@ -84,12 +76,18 @@ $(function(){
 	client.collection('food_requests')
 	.subscribe((requests, changes) => {
 		requests.forEach(request => {
+			var el = document.createElement("div");
+			var div = document.createElement("div");
+			$(el).addClass("marker");
+			$(div).text(request.body.name);
+			$(div).addClass('circle_caption');
 			var image = new Image();
 			console.log(image)
-			image.width = "32";
 			image.src = request.body.image
-			$(image).addClass("img-circle")
-			var myMarker = new CustomMarker(request.body.pos, map, image);
+			$(image).addClass("img-circle marker-image");
+			$(el).append(image);
+			$(el).append(div);
+			var myMarker = new CustomMarker(request.body.pos, map, el);
 		})
   	})
 	$("#request-submit").click(placeFoodRequest);
