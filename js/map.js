30a6573615798d9e3e470669621ec52e74d2a410
Alex <alex.izzedin@gmail.com>
2017-07-29 19:27:11 -0700

    markers

--- js/map.js
+++ js/map.js
@@ -1,4 +1,16 @@
 var map;
+var request_markers = [];
+
+client = Rapid.createClient("NDA1OWE0MWo1b3AzYzA3LnJhcGlkLmlv")
+
+client.collection('food_requests')
+	.subscribe((requests, changes) => {
+		changes.added.forEach(request => {
+			var myLatLng = new google.maps.LatLng(request.body.loc.lat, request.body.loc.lng);
+			var myMarker = new CustomMarker(myLatLng,map);
+	})
+  })
+
 function moveToCurLoc()
 {
 	var pos;
@@ -28,15 +40,32 @@ function initMap() {
 	moveToCurLoc();
 }
 
+
+function placeFoodRequest(){
+	navigator.geolocation.getCurrentPosition(function(position) {
+    	client
+			.collection('food_requests')
+			.newDocument() // generate the id of a new document automatically
+			.mutate({ // create a new to-do and assign it to John
+				name: $("#request-name").val(),
+				time: $("#request-time").val(),
+				food: $("#request-food").val(),
+				loc: {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				}
+			})
+			.then(() => {
+				$(".collapse").collapse('toggle');
+				})
+	})
+}
+
 window.onresize = function() {
 	moveToCurLoc();
 }
 
-$('.collapse').collapse()
-    $(".panel-collapse").on("hide.bs.collapse", function () {
-        $(".panel-collapse-clickable").find('i').removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-    });
 
-    $(".panel-collapse").on("show.bs.collapse", function () {
-        $(".panel-collapse-clickable").find('i').removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-    });
+$(function(){ 
+	$("#request-submit").click(placeFoodRequest);
+})
