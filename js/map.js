6256f09b0f6f48208b98e0c6f9084f268521ec3f
Alex <alex.izzedin@gmail.com>
2017-07-30 02:37:23 -0700

    changed include order

--- js/map.js
+++ js/map.js
@@ -20,45 +20,50 @@ function getCookie(cname) {
     return "";
 }
 
-function moveToCurLoc()
-{
-	var pos;
-	var im = 'circle.png';
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			pos = {
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			}
-			map.setCenter(pos);
-			var userMarker = new google.maps.Marker({
-				position: pos,
-				map: map,
-				icon: im
-			});
-		})
-	}
-}
+ function CustomMarker(pos,  map, image) {
+    this.latlng_ = new google.maps.LatLng(pos.lat, pos.lng);
+    this.image = image
+    this.setMap(map);
+  }
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 18,
-		center: {lat: -34.397, lng: 150.644},
-		disableDefaultUI: true
-	});
-	moveToCurLoc();
-	client.collection('food_requests')
-	.subscribe((requests, changes) => {
-		requests.forEach(request => {
-			image = new Image();
-			image.width = "32";
-			$(image).addClass("img-circle")
-			image.src = request.body.image
-			var myMarker = new CustomMarker(request.body.pos, map, image);
-		})
-  	})
-}
+  CustomMarker.prototype = new google.maps.OverlayView();
+  CustomMarker.prototype.draw = function() {
+    var me = this;
+    var div = this.div_;
+    if (!div) {
+      div = this.div_ = document.createElement('DIV');
+      div.style.border = "none";
+      div.style.position = "absolute";
+      div.style.paddingLeft = "0px";
+      div.style.cursor = 'pointer';
+      //you could/should do most of the above via styling the class added below
+		$(div).append(image);
 
+      google.maps.event.addDomListener(div, "click", function(event) {
+        google.maps.event.trigger(me, "click");
+      });
+
+      var panes = this.getPanes();
+      panes.overlayImage.appendChild(div);
+    }
+
+    var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
+    if (point) {
+      div.style.left = point.x + 'px';
+      div.style.top = point.y + 'px';
+    }
+  };
+
+  CustomMarker.prototype.remove = function() {
+    if (this.div_) {
+      this.div_.parentNode.removeChild(this.div_);
+      this.div_ = null;
+    }
+  };
+
+  CustomMarker.prototype.getPosition = function() {
+   return this.latlng_;
+  };
 
 function placeFoodRequest() {
 	navigator.geolocation.getCurrentPosition(function(position) {
@@ -94,5 +99,15 @@ window.onresize = function() {
 
 
 $(function(){
+	client.collection('food_requests')
+	.subscribe((requests, changes) => {
+		requests.forEach(request => {
+			image = new Image();
+			image.width = "32";
+			$(image).addClass("img-circle")
+			image.src = request.body.image
+			var myMarker = new CustomMarker(request.body.pos, map, image);
+		})
+  	})
 	$("#request-submit").click(placeFoodRequest);
 })
