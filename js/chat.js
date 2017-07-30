4df228b9746627b86528e0111de27f114b29b0b3
Anurag Mittal <anurag.213@gmail.com>
2017-07-30 08:10:18 -0700

    hopefully added chat

---
+++ js/chat.js
@@ -0,0 +1,19 @@
+const client = Rapid.createClient('cmVhNi5yYXBpZC5pbw==')
+
+client
+  .collection('messages')
+  .subscribe((messages, changes) => {
+    changes.added.forEach(message => {
+      $('#chatbox').append($('<div>').text(message.body.text))
+    })
+  })
+
+$('#input').keyup(e => {
+  if (e.which === 13) {
+    client
+      .collection('messages')
+      .newDocument()
+      .mutate({ text: $('#input').val() })
+    $('#input').val('')
+  }
+})
